c6a5348ee7400f197cb6bff8b3c62cdfb9dbc108
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-03 09:56:21 +0800

    feat: add loader to load timeToRead

---
+++ index.js
@@ -0,0 +1,32 @@
+const { getOptions } = require("loader-utils");
+const sanitizeHTML = require("sanitize-html");
+const { words } = require("lodash");
+
+module.exports = function (source, map, meta) {
+  const options = getOptions(this);
+  const speed = options && options.speed ? options.speed : 230;
+
+  const callback = this.async();
+
+  const text =
+    meta.text ||
+    sanitizeHTML(meta.html, {
+      allowedAttributes: {},
+      allowedTags: [],
+    });
+
+  const count = words(text).length;
+  const timeToRead = Math.round(count / speed) || 1;
+
+  const attributes = {
+    ...meta.attributes,
+    timeToRead,
+  };
+
+  const newMeta = {
+    ...meta,
+    text,
+    attributes,
+  };
+  callback(null, source, map, newMeta);
+};
